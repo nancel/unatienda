5eb96dc573f9eebbba25cb695620cb5ca0f80878
Nestor Ancel <nestor.ancel@gmail.com>
2024-02-27 19:54:51 -0300

    styles and sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import { Footer } from "app/components/shared/Footer";
-import { Header } from "app/components/shared/Header";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google'
+import { Header } from 'app/components/shared/Header'
+import { Footer } from 'app/components/shared/Footer'
+import 'app/sass/globals.sass'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+        <Header />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+import styles from "./Hero.module.sass"
+
 export const Hero = () => {
     return (
-        <section>
-            <h1>Hero</h1>  
+        <section className={styles.Hero}>
+            <h1>Una tienda</h1>  
+            <h2>prueba prueba</h2>
         </section>
     )
 }
